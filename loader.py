23040aa7d330b9bdad461b228780b4a9a6965413
Vasiliy <w_av@mail.ru>
2025-12-06 13:42:52 +0300

    init

--- loader.py
+++ loader.py
@@ -98,7 +98,7 @@ def main(args):
             memorysize_format=32,
         )
         response = client.request_download(memory)
-        block_size = int.from_bytes(response.get_payload()[2:])
+        block_size = int.from_bytes(response.get_payload()[2:], byteorder="big")
         block_num = -(-block.size() // block_size)
         print(f"block size: {block_size}, total blocks: {block_num}")
 
