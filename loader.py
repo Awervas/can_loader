67cca9516fa59799336b0b71edb0f244d928a521
Vasiliy <w_av@mail.ru>
2025-12-10 14:48:47 +0300

    can library

--- loader.py
+++ loader.py
@@ -86,7 +86,7 @@ def main(args):
 
     uds_config = udsoncan.configs.default_client_config.copy()
     uds_config['p2_timeout'] = 2
-    uds_config['request_timeout'] = 1
+    uds_config['request_timeout'] = 2
     #
 
     if args.port == 'can0':
@@ -166,6 +166,7 @@ def main(args):
                 try:
                     print(f"Send {i}, attempt: {_ + 1}")
                     client.transfer_data(i & 0xFF, data)
+                    time.sleep(args.transfer_delay)
                     break
                 except TimeoutException:
                     time.sleep(random.random())
